5cf47315d9c2e99351cc6d1be88f685edbfdd4cc
Bafode Camara <gonzalobafode@gmail.com>
2021-02-13 01:29:12 +0300

    initial commit

---
+++ src/pages/post/[id].tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Layout } from "../../components/Layout";
+import { Heading, Box } from "@chakra-ui/react";
+import { useGetPostFromUrl } from "../../utils/useGetPostFromUrl";
+import { EditDeletePostButtons } from "../../components/EditDeletePostButtons";
+import { withApollo } from "../../utils/withApollo";
+
+const Post = ({}) => {
+  const { data, error, loading } = useGetPostFromUrl();
+
+  if (loading) {
+    return (
+      <Layout>
+        <div>loading...</div>
+      </Layout>
+    );
+  }
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
+  if (!data?.post) {
+    return (
+      <Layout>
+        <Box>could not find post</Box>
+      </Layout>
+    );
+  }
+
+  return (
+    <Layout>
+      <Heading mb={4}>{data.post.title}</Heading>
+      <Box mb={4}>{data.post.text}</Box>
+      <EditDeletePostButtons
+        id={data.post.id}
+        creatorId={data.post.creator.id}
+      />
+    </Layout>
+  );
+};
+
+export default withApollo({ ssr: true })(Post);
